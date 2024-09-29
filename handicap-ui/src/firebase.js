23cc075949b562549e0c112d802b4df8b84a6bda
angelNexSpec <90528202+angelcuevaa@users.noreply.github.com>
2024-09-28 17:40:41 -0700

    Update firebase.js

--- handicap-ui/src/firebase.js
+++ handicap-ui/src/firebase.js
@@ -8,7 +8,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  
+  authDomain: "angel-golf-handicap.firebaseapp.com",
+  projectId: "angel-golf-handicap",
+  storageBucket: "angel-golf-handicap.appspot.com",
+  messagingSenderId: "865562785828",
+  appId: "1:865562785828:web:e7b8ca8d8305ed5d2200b1",
+  measurementId: "G-3CFQHQVC1C"
 };
 
 // Initialize Firebase
