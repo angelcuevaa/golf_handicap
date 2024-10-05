87711d35592b4a8ebef26f32aecffb0ecdd75437
angelNexSpec <90528202+angelcuevaa@users.noreply.github.com>
2024-10-05 16:36:44 -0700

    add create golf round

--- handicap-ui/Handicap/NewRound.jsx
+++ handicap-ui/Handicap/NewRound.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import "./NewRound.css"
+import { doc, setDoc, updateDoc, arrayUnion, getDoc, collection, serverTimestamp, writeBatch } from "firebase/firestore"; 
+import { db, auth } from "../src/firebase";
+
 //we want to save a user's round
 //need course, hole count, front or back 9, tees, total score
 const NewRound = () => {
@@ -7,12 +10,50 @@ const NewRound = () => {
     const [roundType, setRoundType] = useState("")
     const [tees, setTees] = useState("")
     const [score, setScore] = useState("")
+    const [slope, setSlope] = useState("")
+    const [rating, setRating] = useState("")
 
 
 
-    const submitRecord = (e)=> {
-        console.log(roundType)
+    const submitRecord = async (e)=> {
         e.preventDefault()
+
+        try{
+            const batch = writeBatch(db);
+
+            //set timestamp in user collection
+            const userRef = doc(db, 'users', auth.currentUser.uid);
+            batch.set(userRef, {timestamp: serverTimestamp() });
+            
+            //create new document in scorecard collection with new scorecard details
+            const scorecardRef = collection(userRef, "scorecards");
+            const newDocRef = doc(scorecardRef);
+            batch.set(newDocRef, {courseName: courseName, score: score, tees: tees, timestamp: serverTimestamp()});
+
+
+            await batch.commit();
+            console.log("success")
+        }
+        catch(e){
+            console.log("Submitted another scorecard too fast!")
+        }
+            
+        
+
+        // updateDoc(userRef, {
+        //     scores: arrayUnion({courseName: courseName, score: score, tees: tees })
+        // });
+
+        // setDoc(userRef, { scores:[{courseName: courseName, score: score, tees: tees}] }, { merge: true });
+
+        // const docRef = doc(db, "users", auth.currentUser.uid);
+        // const docSnap = await getDoc(docRef);
+        // if (docSnap.exists()) {
+        //     console.log("Document data:", docSnap.data());
+        //   } else {
+        //     // docSnap.data() will be undefined in this case
+        //     console.log("No such document!");
+        //   }
 
     }
     return(
@@ -26,24 +67,31 @@ const NewRound = () => {
                     <p>Round Type</p>
                     <input type="radio" name="roundType" value="full" onChange={(e) => setRoundType(e.target.value)}/>
                     <label>18 holes</label>
-                    <input type="radio" name="roundType" value="front" onChange={(e) => setRoundType(e.target.value)}/>
-                    <label>Front 9</label>
-                    <input type="radio" name="roundType" value="back" onChange={(e) => setRoundType(e.target.value)}/>
-                    <label>Back 9</label>
+                    <input type="radio" name="roundType" value="nine" onChange={(e) => setRoundType(e.target.value)}/>
+                    <label>9 holes</label>
+                   
                 </div>
-                <div className="newRound-tees">
+                {/* <div className="newRound-tees">
                     <p>Tees</p>
                     <select className="newRound-select" value={tees} onChange={(e) => setTees(e.target.value)}>
                         <option>Red</option>
                         <option>White</option>
                         <option>Blue</option>
                     </select>
+                </div> */}
+                <div>
+                    <p>Rating</p>
+                    <input className="newRound-input" value={score} onChange={(e) => setRating(e.target.value)}/>
+                </div>
+                <div>
+                    <p>Slope</p>
+                    <input className="newRound-input" value={score} onChange={(e) => setSlope(e.target.value)}/>
                 </div>
                 <div>
                     <p>Score</p>
                     <input className="newRound-input" value={score} onChange={(e) => setScore(e.target.value)}/>
                 </div>
-                <button onClick={(e) => submitRecord(e)}>Submit</button>
+                <button onClick={async (e) => await submitRecord(e)}>Submit</button>
             </form>
         </div>
     )
--- handicap-ui/src/firebase.js
+++ handicap-ui/src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,6 +22,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
+const db = getFirestore(app);
 
-export {auth}
+
+export {auth, db}
 //const analytics = getAnalytics(app);
